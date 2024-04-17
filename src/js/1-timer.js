57e2b4c3bcd42485a93fc26eaee9a9ade2876ec8
smelik81 <artem.smelyanskiy81@gmail.com>
2024-04-17 16:21:42 +0300

    finished style css and create some function

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,87 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const LS_KEY = "show date";
+const inputEl = document.querySelector('#datetime-picker');
+const btnElStart = document.querySelector('button[data-start]');
+btnElStart.disabled = true;
+const dataDay = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+
+let userSelectedDate;
+userSelectedDate = btnElStart.ariaDisabled;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] <= Date.now()) {
+          iziToast.error({
+            title: 'Error',
+            titleSize: '18',
+            titleColor: 'black',
+            message: 'Please choose a date in the future',
+            position: 'topRight',
+            messageColor: 'white',
+            messageSize: '18',
+            backgroundColor: 'red',
+            maxWidth: '380',
+            transitionIn: 'fadeInLeft',
+            timeout: '3000',
+          });
+            btnElStart.disabled = true;
+            btnElStart.classList.add('not-show');
+        } else {
+            btnElStart.disabled = false;
+            btnElStart.classList.remove('not-show');
+            userSelectedDate = selectedDates[0];
+      }
+  },
+};
+
+flatpickr(inputEl, options);
+
+
+
+
+btnElStart.addEventListener('click', showCountdawn);
+function showCountdawn(event) {
+    console.log(event.target.dataset.start);
+}
+
+
+
+
+
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+};
+
+function convertMs(ms) {
+  const second = 1000; // 1000
+  const minute = second * 60; // 60 000
+  const hour = minute * 60; // 3 600 000
+  const day = hour * 24; // 84 400 000
+
+  
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
+
+/* console.log(convertMs(2000)); 
+console.log(convertMs(140000)); 
+console.log(convertMs(24140000)); 
+ */
