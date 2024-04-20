a9b3ab09c741a66e407a27d2d0c76cc5fb078d64
smelik81 <artem.smelyanskiy81@gmail.com>
2024-04-21 01:43:48 +0300

    add file css and finished task-2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,3 +2,54 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const element = event.currentTarget.elements;
+    const delay = Number(element.delay.value);
+    const state = element.state.value;
+    console.log(state);
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+                console.log(`✅ Fulfilled promise in ${delay}ms`);
+            } else {
+                reject(delay);
+                console.log(`❌ Rejected promise in ${delay}ms`);
+            }
+        })
+    }, delay);
+
+
+    promise
+        .then((delay) => {
+            iziToast.show({
+                position: 'topRight',
+                titleColor: 'white',
+                titleSize: '18px',
+                backgroundColor: '#59a10d',
+                messageColor: 'white',
+                messageSize: '16px',
+                title: 'OK',
+                message: `✅ Fulfilled promise in ${delay}ms`,
+            })
+        })
+        .catch((delay) => {
+            iziToast.show({
+                position: 'topRight',
+                titleColor: "white",
+                titleSize: '18px',
+                backgroundColor: '#b54657',
+                messageColor: "white",
+                messageSize: '16px',
+                title: `Error`,
+                message: `❌ Rejected promise in ${delay}ms`
+            })
+        })
+    
+};
